ac6c1ac405bb0f6d3f30b24c9937d58bcdd0ad42
Danil Ochagov <danilochagov@gmail.com>
2021-02-06 20:43:28 +0300

    Add some enhancements

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import json
+import threading
 from playsound import playsound
 
 
@@ -26,30 +27,54 @@ response = session.put("https://ticket.bolshoi.ru/api/v1/client/login", headers=
 if response.status_code != 200:
     exit()
 
-# Requesting JSON data (seats)
-url = input("Введите ссылку, по которой будет проходить мониторинг билетов: ").strip()
-json_url = url.replace("show", "api/v1/client/shows") # Format to JSON URL
-print("Приложение работает...(просьба не закрывать это окно)")
+
+# Collecting user data
+number_of_urls = int(input("Введите количество ссылок (число), по которым будет происходить мониторинг: "))
+json_url_array = []
+
+for i in range(number_of_urls):
+    url = input("Введите ссылку, по которой будет проходить мониторинг билетов: ").strip()
+    json_url = url.replace("show", "api/v1/client/shows") # Format to JSON URL
+    json_url_array.append(json_url)
 
 
-while True:
-    try:
-        # Sending requests in order to get two responses (old and new)
-        response = session.get(json_url, headers=headers, data=data, verify=False)
-        json_data = json.loads(response.text)
-        number_of_seats = json_data["showInfo"]["freeSeats"]
+def work(url):
+    """ Requesting JSON data (free seats) """
+    print("Запускаем мониторинг: " + url) 
 
-        time.sleep(interval)
+    while True:
+        try:
+            # Sending requests in order to get two responses (old and new)
+            response = session.get(url, headers=headers, data=data, verify=False)
+            json_data = json.loads(response.text)
+            number_of_seats = json_data["showInfo"]["freeSeats"]
 
-        response = session.get(json_url, headers=headers, data=data, verify=False)
-        json_data = json.loads(response.text)
-        new_number_of_seats = json_data["showInfo"]["freeSeats"]
+            time.sleep(interval)
 
-        if new_number_of_seats > number_of_seats:
-            playsound("notification.mp3")
-            print("Новые билеты!") 
-            continue
-        else:
-            continue
-    except Exception:
-        break
+            response = session.get(url, headers=headers, data=data, verify=False)
+            json_data = json.loads(response.text)
+            new_number_of_seats = json_data["showInfo"]["freeSeats"]
+
+            if new_number_of_seats > number_of_seats:
+                print("Новые билеты по " + url)
+
+                for i in range(3):
+                    playsound("success.mp3")
+                    time.sleep(1) 
+        except Exception:
+            print("\n\n!!!ВОЗНИКЛА НЕИЗВЕСТНАЯ ОШИБКА!!!")
+
+            for i in range(5):
+                playsound("error.mp3")
+                time.sleep(1)
+
+            exit()
+
+
+# Spawning our threads
+for i in json_url_array:
+    t = threading.Thread(target=work, args=(i,))
+    t.start()
+    time.sleep(1)
+
+print("Приложение работает...(просьба не закрывать окно)")
